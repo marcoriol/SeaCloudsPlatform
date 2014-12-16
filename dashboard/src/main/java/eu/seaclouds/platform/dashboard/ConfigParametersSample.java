e89a067b7327c05654d2fea1ad7c80be309b6609
Miguel Barrientos <mbarrientos@lcc.uma.es>
2014-12-16 14:19:55 +0100

    Dashboard: - Moving all js libraries to js/lib folder. - Renamed ConfigParameters to ConfigParametersSample. Now it needs to be renamed to ConfigParameter to keep local configuration out of CVS

--- dashboard/src/main/java/eu/seaclouds/platform/dashboard/ConfigParametersSample.java
+++ dashboard/src/main/java/eu/seaclouds/platform/dashboard/ConfigParametersSample.java
@@ -2,8 +2,10 @@ package eu.seaclouds.platform.dashboard;
 
 /**
  * Created by Adrian on 14/11/2014.
+ *
+ * Please, rename this file to ConfigParameters.java.
  */
-public interface ConfigParameters {
+public interface ConfigParametersSample {
     public static String MONITOR_ENDPOINT = "http://localhost:8081";    // CHANGE THIS
     public static String DEPLOYER_ENDPOINT = "http://localhost:8081";   // CHANGE THIS
     public static String SLA_ENDPOINT = "http://localhost:8082";        // CHANGE THIS
